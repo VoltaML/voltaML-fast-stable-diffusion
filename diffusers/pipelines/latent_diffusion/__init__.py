3246de971fce94c15baa7b4fa499d3d543e85557
kamalkraj <kamalraj97@gmail.com>
2022-11-27 12:14:34 +0530

    rm cache,files

--- diffusers/pipelines/latent_diffusion/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-from ...utils import is_transformers_available
-from .pipeline_latent_diffusion_superresolution import LDMSuperResolutionPipeline
-
-
-if is_transformers_available():
-    from .pipeline_latent_diffusion import LDMBertModel, LDMTextToImagePipeline
