9c99db70149c71678d60b7ec9c0885e6b16d078c
Stax124 <tamoncz@gmail.com>
2023-01-25 21:59:11 +0100

    Partial fix: Image browser scaling

--- frontend/src/views/ImageBrowserView.vue
+++ frontend/src/views/ImageBrowserView.vue
@@ -1,95 +1,90 @@
 <template>
-  <NCard class="main-card">
-    <NGrid cols="1 850:3" style="height: calc(94vh - 200px - 64px)">
-      <NGi span="2">
-        <NCard style="width: 100%; height: 100%; display: inline-flex">
-          <NImage
-            v-if="global.state.imageBrowser.currentImage.path !== ''"
-            class="img-container"
-            :src="imageSrc"
-            object-fit="contain"
-            style="
-              width: 100%;
-              height: 100%;
-              justify-content: center;
-              max-height: 61vh;
-            "
-          />
-        </NCard>
-      </NGi>
-      <NGi>
-        <NCard style="height: 100%">
+  <NGrid cols="1 850:3">
+    <NGi span="2" style="height: calc(100vh - 200px - 53px - 24px)">
+      <NImage
+        v-if="global.state.imageBrowser.currentImage.path !== ''"
+        :src="imageSrc"
+        object-fit="contain"
+        style="width: 100%; height: 100%; justify-content: center"
+        :img-props="{ style: { maxWidth: '95%', maxHeight: '95%' } }"
+      />
+    </NGi>
+    <NGi>
+      <NGrid cols="2">
+        <NGi span="2">
           <NCard title="Prompt" size="small">{{
             global.state.imageBrowser.currentImageMetadata.prompt
           }}</NCard>
+        </NGi>
+
+        <NGi span="2">
           <NCard title="Negative Prompt" size="small">{{
             global.state.imageBrowser.currentImageMetadata.negative_prompt
           }}</NCard>
-          <NGrid cols="2">
-            <NGi>
-              <NCard title="Width" size="small">{{
-                global.state.imageBrowser.currentImageMetadata.width
-              }}</NCard>
-            </NGi>
-            <NGi>
-              <NCard title="Height" size="small">{{
-                global.state.imageBrowser.currentImageMetadata.height
-              }}</NCard>
-            </NGi>
-          </NGrid>
-          <NGrid cols="2">
-            <NGi>
-              <NCard title="Steps" size="small">{{
-                global.state.imageBrowser.currentImageMetadata.steps
-              }}</NCard>
-            </NGi>
-            <NGi>
-              <NCard title="Guidance Scale" size="small">{{
-                global.state.imageBrowser.currentImageMetadata.guidance_scale
-              }}</NCard>
-            </NGi>
-          </NGrid>
-          <NGrid cols="2">
-            <NGi>
-              <NCard title="Seed" size="small">{{
-                global.state.imageBrowser.currentImageMetadata.seed
-              }}</NCard>
-            </NGi>
-            <NGi>
-              <NCard title="Model" size="small">{{
-                global.state.imageBrowser.currentImageMetadata.model
-              }}</NCard>
-            </NGi>
-          </NGrid>
-        </NCard>
-      </NGi>
-    </NGrid>
-    <NCard style="height: 200px">
-      <NTabs type="segment">
-        <NTabPane name="Txt2Img">
-          <NScrollbar x-scrollable trigger="hover">
-            <div style="white-space: nowrap">
-              <span
-                v-for="(i, index) in txt2imgData"
-                @click="txt2imgClick(index)"
-                v-bind:key="index"
-                class="img-container"
-              >
-                <NImage
-                  class="img-slider"
-                  :src="urlFromPath(i.path)"
-                  lazy
-                  preview-disabled
-                  style="justify-content: center"
-                />
-              </span>
-            </div>
-          </NScrollbar>
-        </NTabPane>
-        <NTabPane name="Img2Img"> </NTabPane>
-      </NTabs>
-    </NCard>
-  </NCard>
+        </NGi>
+
+        <NGi>
+          <NCard title="Width" size="small">{{
+            global.state.imageBrowser.currentImageMetadata.width
+          }}</NCard>
+        </NGi>
+        <NGi>
+          <NCard title="Height" size="small">{{
+            global.state.imageBrowser.currentImageMetadata.height
+          }}</NCard>
+        </NGi>
+
+        <NGi>
+          <NCard title="Steps" size="small">{{
+            global.state.imageBrowser.currentImageMetadata.steps
+          }}</NCard>
+        </NGi>
+        <NGi>
+          <NCard title="Guidance Scale" size="small">{{
+            global.state.imageBrowser.currentImageMetadata.guidance_scale
+          }}</NCard>
+        </NGi>
+
+        <NGi>
+          <NCard title="Seed" size="small">{{
+            global.state.imageBrowser.currentImageMetadata.seed
+          }}</NCard>
+        </NGi>
+        <NGi>
+          <NCard title="Model" size="small">{{
+            global.state.imageBrowser.currentImageMetadata.model
+          }}</NCard>
+        </NGi>
+      </NGrid>
+    </NGi>
+    <NGi span="3">
+      <NCard style="height: 200px">
+        <NTabs type="segment">
+          <NTabPane name="Txt2Img">
+            <NScrollbar x-scrollable trigger="hover">
+              <div style="white-space: nowrap">
+                <span
+                  v-for="(i, index) in txt2imgData"
+                  @click="txt2imgClick(index)"
+                  v-bind:key="index"
+                  class="img-container"
+                >
+                  <NImage
+                    class="img-slider"
+                    :src="urlFromPath(i.path)"
+                    lazy
+                    preview-disabled
+                    style="justify-content: center"
+                  />
+                </span>
+              </div>
+            </NScrollbar>
+          </NTabPane>
+          <NTabPane name="Img2Img"> </NTabPane>
+        </NTabs>
+      </NCard>
+    </NGi>
+  </NGrid>
 </template>
 
 <script lang="ts" setup>
@@ -143,9 +138,6 @@ fetch(`${serverUrl}/api/output/txt2img`)
 </script>
 
 <style scoped>
-.main-card {
-  height: 94vh;
-}
 .img-slider {
   aspect-ratio: 1/1;
   height: 100px;
