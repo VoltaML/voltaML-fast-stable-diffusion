8d4d2ad556531e76e76d941670c63bc69d37dff4
Stax124 <tamoncz@gmail.com>
2023-01-31 11:13:52 +0100

    Fix cache_dir problems

--- core/config/__init__.py
+++ core/config/__init__.py
@@ -1,5 +1,8 @@
+import os
+
 from .config import Configuration, Img2ImgConfig, Txt2ImgConfig
 
 config = Configuration()
+config.cache_dir = os.environ.get("DIFFUSERS_CACHE", config.cache_dir)
 
 __all__ = ["config", "Img2ImgConfig", "Txt2ImgConfig", "Configuration"]
