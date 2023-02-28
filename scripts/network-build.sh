dddfa24c3ec77af3d3ef01fae0eaa27fb684fe46
Stax124 <tamoncz@gmail.com>
2023-02-28 18:08:05 +0100

    Add new requirement, update docker build scripts

--- scripts/network-build.sh
+++ scripts/network-build.sh
@@ -1,3 +1,3 @@
-docker build -t voltaml_network -f ./dockerfiles/network . \
+docker build -t voltaml_network -f ./dockerfiles/dockerfile.network . \
 && docker tag voltaml_network:latest voltaml/volta_diffusion_webui:latest \
 && docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
