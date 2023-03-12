957d95238cafb635eb60ca63fe8c61af1014aa9c
Stax124 <Stax124@users.noreply.github.com>
2023-03-12 17:45:12 +0000

    deploy: 272939e0a9c4baa047f46a0e48a685b6775de168

---
+++ assets/troubleshooting_linux.md.590a22a0.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o}from"./app.1d8f81f9.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"troubleshooting/linux.md","lastUpdated":1678643075000}'),a={name:"troubleshooting/linux.md"};function n(r,s,c,i,l,p){return o(),e("div")}const u=t(a,[["render",n]]);export{_ as __pageData,u as default};
