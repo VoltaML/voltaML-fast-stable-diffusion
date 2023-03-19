ad28c6c671bb50976182e8c49901467a3c1f9909
Stax124 <tamoncz@gmail.com>
2023-03-19 16:08:08 +0100

    Move Dockerfile into root dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
-docker build -t volta -f ./dockerfiles/dockerfile . \
+docker build -t volta -f ./dockerfile . \
 && docker tag volta:latest stax124/volta:experimental \
 && docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
