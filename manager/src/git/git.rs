08892ca92f8ead07800c74a57ec37c97e8b0d7d2
Stax124 <tamoncz@gmail.com>
2023-05-16 16:04:15 +0200

    Manager: Better handle Git when dir is not available, remove potentional panic points

--- manager/src/git/git.rs
+++ manager/src/git/git.rs
@@ -3,16 +3,16 @@ use std::error::Error;
 use crate::utils::shell::{run_command, spawn_command};
 
 pub fn is_git_repo_up_to_date() -> Result<bool, Box<dyn Error>> {
-    spawn_command("git fetch", "Fetch git changes")?;
+    run_command("git fetch", "")?;
 
     let current_branch_raw =
         run_command(&"git rev-parse --abbrev-ref HEAD", "Get current git branch")?;
     let current_branch = current_branch_raw.trim();
 
     let remote_command = format!("git rev-parse origin/{}", current_branch);
-    let remote_commit = run_command(&remote_command, "Check if git repo is up-to-date")?;
+    let remote_commit = run_command(&remote_command, "Parse remote commit hash")?;
 
-    let local_commit_raw = spawn_command("git rev-parse HEAD", "Check if git repo is up-to-date")?;
+    let local_commit_raw = run_command("git rev-parse HEAD", "Parse local commit hash")?;
     let local_commit = local_commit_raw.trim();
 
     Ok(remote_commit == local_commit)
