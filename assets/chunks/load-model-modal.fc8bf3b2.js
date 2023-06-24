9ce760de2d3f927d6f5edb357e673c57b17f8b4d
Stax124 <Stax124@users.noreply.github.com>
2023-06-24 12:27:38 +0000

    deploy: 553a9b946c1ed9e8277a9142494fda98452ca2b2

---
+++ assets/chunks/load-model-modal.fc8bf3b2.js
@@ -0,0 +1,1 @@
+const s="/voltaML-fast-stable-diffusion/assets/model-loader-button.0d8be217.webp",o="/voltaML-fast-stable-diffusion/assets/load-model-modal.64cb8825.webp";export{s as _,o as a};
--- pr-preview/pr-88/assets/chunks/manager-github-release.d1e11f31.js
+++
@@ -1,1 +0,0 @@
-const s="/voltaML-fast-stable-diffusion/assets/manager-github-release.ecb81457.webp";export{s as _};
