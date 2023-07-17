aeccce109e574bf0747dc4c1adeaae8f823efc1c
gabe56f <gabe@iceyleagons.net>
2023-07-17 20:54:13 +0200

    Fix LWP

--- core/inference/pytorch/lwp.py
+++ core/inference/pytorch/lwp.py
@@ -315,84 +315,94 @@ def get_weighted_text_embeddings(
     if isinstance(prompt, str):
         prompt = [prompt]
 
-    loralist = []
-    for l in pipe.loras:
-        loralist.append(l)
-    for i, prompt_ in enumerate(prompt):
-        prompt[i], load_map = parse_prompt_special(prompt_)
-        if len(load_map.keys()) != 0:
-            logger.debug(load_map)
-            if "lora" in load_map:
-                from ..lora import install_lora_hook
+    if not hasattr(pipe, "clip_inference"):
+        loralist = []
+        for l in pipe.loras:
+            loralist.append(l)
+        for i, prompt_ in enumerate(prompt):
+            prompt[i], load_map = parse_prompt_special(prompt_)
+            if len(load_map.keys()) != 0:
+                logger.debug(load_map)
+                if "lora" in load_map:
+                    from ..lora import install_lora_hook
 
-                install_lora_hook(pipe)
+                    install_lora_hook(pipe)
 
-                if not hasattr(pipe.lora_injector, "old_containers"):
-                    pipe.lora_injector.old_containers = None
-                if pipe.lora_injector.old_containers is None:
-                    pipe.lora_injector.old_containers = pipe.lora_injector.containers
-                    logger.debug(f"Old containers: {pipe.lora_injector.old_containers}")
-
-                for lora, alpha in load_map["lora"]:
-                    correct_path = get_full_model_path(
-                        lora, model_folder="lora", force=True
-                    )
-                    for ext in [".safetensors", ".ckpt", ".bin", ".pt", ".pth"]:
-                        path = get_full_model_path(
-                            lora + ext, model_folder="lora", force=True
+                    if not hasattr(pipe.lora_injector, "old_containers"):
+                        pipe.lora_injector.old_containers = None
+                    if pipe.lora_injector.old_containers is None:
+                        pipe.lora_injector.old_containers = (
+                            pipe.lora_injector.containers
                         )
-                        if path.exists():
-                            correct_path = path
-                            break
-                    loralist.append((correct_path, alpha))
-            elif "ti" in load_map:
-                # Disable TI for now as there's no reliable way to unload them
-                logger.info("Textual inversion via prompts is temporarily disabled.")
-                continue
-                for ti in load_map["ti"]:  # type: ignore
-                    ti: str
-                    correct_path = get_full_model_path(
-                        ti, model_folder="textual-inversion", force=True
-                    )
-                    for ext in [".safetensors", ".ckpt", ".bin", ".pt", ".pth"]:
-                        path = get_full_model_path(
-                            ti + ext, model_folder="textual-inversion", force=True
+                        logger.debug(
+                            f"Old containers: {pipe.lora_injector.old_containers}"
                         )
-                        if path.exists():
-                            correct_path = path
-                            break
-                    logger.debug(f"Applying textual inversion {correct_path.name}")
-                    pipe.load_textual_inversion(correct_path.absolute().as_posix())
 
-    if config.api.huggingface_style_parsing and hasattr(pipe, "lora_injector"):
-        for prompt_ in prompt:
-            old_l = loralist
-            loralist = list(
-                filter(
-                    lambda x: Path(x[0]).stem.casefold()
-                    in prompt_.casefold(),  # pylint: disable=cell-var-from-loop
-                    loralist,
+                    for lora, alpha in load_map["lora"]:
+                        correct_path = get_full_model_path(
+                            lora, model_folder="lora", force=True
+                        )
+                        for ext in [".safetensors", ".ckpt", ".bin", ".pt", ".pth"]:
+                            path = get_full_model_path(
+                                lora + ext, model_folder="lora", force=True
+                            )
+                            if path.exists():
+                                correct_path = path
+                                break
+                        loralist.append((correct_path, alpha))
+                elif "ti" in load_map:
+                    # Disable TI for now as there's no reliable way to unload them
+                    logger.info(
+                        "Textual inversion via prompts is temporarily disabled."
+                    )
+                    continue
+                    for ti in load_map["ti"]:  # type: ignore
+                        ti: str
+                        correct_path = get_full_model_path(
+                            ti, model_folder="textual-inversion", force=True
+                        )
+                        for ext in [".safetensors", ".ckpt", ".bin", ".pt", ".pth"]:
+                            path = get_full_model_path(
+                                ti + ext, model_folder="textual-inversion", force=True
+                            )
+                            if path.exists():
+                                correct_path = path
+                                break
+                        logger.debug(f"Applying textual inversion {correct_path.name}")
+                        pipe.load_textual_inversion(correct_path.absolute().as_posix())
+
+        if config.api.huggingface_style_parsing and hasattr(pipe, "lora_injector"):
+            for prompt_ in prompt:
+                old_l = loralist
+                loralist = list(
+                    filter(
+                        lambda x: Path(x[0]).stem.casefold()
+                        in prompt_.casefold(),  # pylint: disable=cell-var-from-loop
+                        loralist,
+                    )
                 )
-            )
-            for lora, weight in old_l:
-                if (lora, weight) not in loralist:
-                    try:
-                        pipe.remove_lora(Path(lora).name)
-                        logger.debug(f"Unloading LoRA: {Path(lora).name}")
-                    except KeyError:
-                        pass
-    if hasattr(pipe, "lora_injector"):
-        remove = pipe.unload_loras
-        for lora, alpha in loralist:
-            name = Path(lora).name
-            if name not in pipe.lora_injector.containers:
-                logger.debug(f"{pipe.loras}, {loralist}")
-                if lora not in map(lambda x: x[0], pipe.loras) and name not in remove:
-                    logger.debug(f"Adding LoRA {name} to the removal list")
-                    remove.append(name)
-                logger.debug(f"Applying LoRA {name} with strength {alpha}")
-                pipe.apply_lora(lora, alpha)
-        pipe.unload_loras = remove
+                for lora, weight in old_l:
+                    if (lora, weight) not in loralist:
+                        try:
+                            pipe.remove_lora(Path(lora).name)
+                            logger.debug(f"Unloading LoRA: {Path(lora).name}")
+                        except KeyError:
+                            pass
+        if hasattr(pipe, "lora_injector"):
+            remove = pipe.unload_loras
+            for lora, alpha in loralist:
+                name = Path(lora).name
+                if name not in pipe.lora_injector.containers:
+                    logger.debug(f"{pipe.loras}, {loralist}")
+                    if (
+                        lora not in map(lambda x: x[0], pipe.loras)
+                        and name not in remove
+                    ):
+                        logger.debug(f"Adding LoRA {name} to the removal list")
+                        remove.append(name)
+                    logger.debug(f"Applying LoRA {name} with strength {alpha}")
+                    pipe.apply_lora(lora, alpha)
+            pipe.unload_loras = remove
 
     if not skip_parsing:
         prompt_tokens, prompt_weights = get_prompts_with_weights(
