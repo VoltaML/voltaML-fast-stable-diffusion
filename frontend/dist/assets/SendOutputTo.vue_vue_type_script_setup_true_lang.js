5d7ce42b1d7fa18d27c96e52993245beac7a6e6d
Tomáš Novák <tomas.novak@olc.cz>
2023-10-19 12:34:39 +0200

    Fix ImageBrowser sampler being incorrectly displayed, rebuild frontend

--- frontend/dist/assets/SendOutputTo.vue_vue_type_script_setup_true_lang.js
+++ frontend/dist/assets/SendOutputTo.vue_vue_type_script_setup_true_lang.js
@@ -1,4 +1,4 @@
-import { d as defineComponent, o as openBlock, a as createElementBlock, b as createBaseVNode, bH as useRouter, u as useSettings, v as useState, r as ref, ba as reactive, O as watch, c as computed, e as createVNode, w as withCtx, f as unref, N as NCard, h as NButton, i as createTextVNode, Q as NScrollbar, F as Fragment, g as renderList, t as toDisplayString, bf as NDivider, m as NModal, k as createBlock, A as NGi, H as NGrid, G as createCommentVNode } from "./index.js";
+import { d as defineComponent, o as openBlock, a as createElementBlock, b as createBaseVNode, bI as useRouter, u as useSettings, v as useState, r as ref, ba as reactive, O as watch, c as computed, e as createVNode, w as withCtx, f as unref, N as NCard, h as NButton, i as createTextVNode, Q as NScrollbar, F as Fragment, g as renderList, t as toDisplayString, bf as NDivider, m as NModal, k as createBlock, A as NGi, H as NGrid, G as createCommentVNode } from "./index.js";
 import { a as NSwitch } from "./Switch.js";
 const _hoisted_1$3 = {
   xmlns: "http://www.w3.org/2000/svg",
@@ -223,7 +223,12 @@ const _sfc_main = /* @__PURE__ */ defineComponent({
     }
     return (_ctx, _cache) => {
       return openBlock(), createElementBlock(Fragment, null, [
-        createVNode(unref(NModal), { show: showModal.value }, {
+        createVNode(unref(NModal), {
+          show: showModal.value,
+          "onUpdate:show": _cache[1] || (_cache[1] = ($event) => showModal.value = $event),
+          "mask-closable": "",
+          "close-on-esc": ""
+        }, {
           default: withCtx(() => [
             createVNode(unref(NCard), {
               style: { "max-width": "700px" },
